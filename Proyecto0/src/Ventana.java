25996b07c8404d5620b9830a6ee14b75dc086b7c
Alejandro <Alejandro@Alejandro-PC>
2015-09-04 10:54:47 -0300

    Al proyecto que estaba creado, le exporté el jar.

--- Proyecto0/src/Ventana.java
+++ Proyecto0/src/Ventana.java
@@ -1,7 +1,5 @@
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -9,7 +7,6 @@ import javax.swing.JTextField;
 
 import javax.swing.WindowConstants;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.SwingUtilities;
 
